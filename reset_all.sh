b8c020b1b1c5cc7e9848efc78e5c1163acdf8175
hector <hector@hector-laptop.(none)>
2008-05-15 00:16:45 +0200

    reset_all.sh ok

--- reset_all.sh
+++ reset_all.sh
@@ -1,6 +1,5 @@
+ruby -e 'a=""; 140.times{|n| a=a+rand(9).to_s};File.open("config/secret","w"){|f| f.puts a}'
 mkdir log
-a=""; 140.times{|n| a=a+rand(9).to_s}
-IO.write("config/secret","w"){|f| f.puts a}
 rake db:drop:all
 rake db:create:all
 rake db:migrate
