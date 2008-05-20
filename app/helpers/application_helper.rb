3141e7e4969c79ecdb863fe6986b7ceca2cdfd90
Héctor Pérez <hecpeare@gmail.com>
2008-05-21 01:26:34 +0200

    add helper link_to_inhabitant

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_to_inhabitant(i)
+    link_to i.to_s, "/#{i.to_param}"
+  end
 end
