124e65452295178416bdf2f2b4c06fba246cf674
Héctor Pérez <hecpeare@gmail.com>
2008-07-20 21:13:43 +0200

    add xml images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,19 @@ module ApplicationHelper
   def link_to_abitant(i)
     link_to i.to_s, "/#{i.to_param}"
   end
+  def link_to_xml(url=request.request_uri.to_s)
+    link_to(image_tag("xml.gif",:border=>0), add_format_to_url(url,"xml"))
+  end
+  def add_format_to_url(url,ext)
+    if url.class==String
+      if url =~ /\?/
+        url="#{$`}.#{ext}?#{$'}"
+      else
+        url=url + "." + ext
+      end
+    else
+      url=url.merge(:format=>:xml)
+    end
+  end
+
 end
